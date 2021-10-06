7485c86494f09e2e5e573596aca269095e45e74b
Sanekakf <83131846+sanekakf@users.noreply.github.com>
2021-10-06 11:57:02 +0300

    Update register.php

--- register/register.php
+++ register/register.php
@@ -4,11 +4,15 @@ $login = $_POST['login'];
 $pass = $_POST['password'];
 require "../info.php";
 $conn = new PDO($dsn);
-$res = $conn->query("INSERT INTO users (id, login, password) VALUES($id, '$login', '$pass')");
-if (!$res){
-    header(" Location: /error?id=174");
-} else {
+$rs = $conn->query("SELECT * FROM users WHERE login = '$login'");
+$s = $rs->fetchAll(PDO::FETCH_ASSOC);
+if ($s){
+    header("Location: /error/?id=174");
+    exit();
+}
+else {
+    $res = $conn->query("INSERT INTO users (id, login, password) VALUES($id, '$login', '$pass')");
     setcookie("user", $login, time() + 3600, '/');
-    header("Location: /");   
+    header("Location: /home");
 }
 ?>
