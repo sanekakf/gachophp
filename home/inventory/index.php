2e1d17c31346169b757bb2cdfa3ee64d485d3a26
Sanekakf <83131846+sanekakf@users.noreply.github.com>
2021-10-08 23:07:57 +0300

    Create index.php

---
+++ home/inventory/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $_GET['name']?></title>
+    <?php require "../htmls/styles.php" ?>
+</head>
+<body>
+<div class=" container py-3"
+<?php require "../htmls/header.php" ?>
+    
+    <h1>В разработке...</h1>
+    
+<?php require "../htmls/footer.php" ?>
+</div>
+</body>
+</html>
